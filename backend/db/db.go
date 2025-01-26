51570a87ffd63826236748bad4f40899a7375e37
marj4 <mkasumov86@gmail.com>
2025-01-26 12:15:31 +0300

    1. Добавил метод Connect2 в db.go, для подключения к серверу Redis 2.

--- backend/db/db.go
+++ backend/db/db.go
@@ -4,6 +4,7 @@ import (
 	error2 "Market/error"
 	"database/sql"
 	_ "github.com/lib/pq"
+	"github.com/redis/go-redis/v9"
 )
 
 func Connect(DatabaseURL string) (*sql.DB, error) {
@@ -18,4 +19,9 @@ func Connect(DatabaseURL string) (*sql.DB, error) {
 
 	return db, nil
 
+}
+
+func Connect2(ResidServerURL string) *redis.Client {
+	rdb := redis.NewClient((&redis.Options{Addr: ResidServerURL, DB: 0}))
+	return rdb
 }
--- config/config.go
+++ config/config.go
@@ -7,18 +7,25 @@ import (
 )
 
 type Config struct {
-	DatabaseURL string
-	Port        string
+	DatabaseURL  string //Адрес ДБ
+	Port         string //Порт БД
+	Email        string //Почта откуда будут отправляться коды верификации
+	App_Password string //Пароль приложения для подключения к SMTP серверу(код верификации)
+	Redis_Server string //Адрес сервера-Redis, в котором будут храниться временные данные
 }
 
 func LoadConfig() (*Config, error) {
+
 	if err := godotenv.Load("File.env"); err != nil {
 		return nil, error2.Wrap("Failed load .env file", err)
 	}
 
 	return &Config{
-		DatabaseURL: os.Getenv("DATABASE"),
-		Port:        os.Getenv("SERVER_PORT"),
+		DatabaseURL:  os.Getenv("DATABASE"),
+		Port:         os.Getenv("SERVER_PORT"),
+		Email:        os.Getenv("EMAIL"),
+		App_Password: os.Getenv("APP_PASSWORD"),
+		Redis_Server: os.Getenv("REDIS_SERVER"),
 	}, nil
 
 }
